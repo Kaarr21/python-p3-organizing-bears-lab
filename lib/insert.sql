16bb107df515f33e8757f68cde1b3a216612252d
Karoki <wanjikukar19@gmail.com>
2025-05-20 20:26:30 +0300

    SQL

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES('Mr.Chocolate', 20, 'M', 'black', 'mild', 0);
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES('Rowdy', 10, 'M', 'brown', 'mild', 1);
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES('Tabitha', 6, 'F', 'white', 'mild', 1);
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES('Sergent Brown', 19 'M', 'black', 'wild', 0);
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES('Melissa', 13, 'F', 'brown', 'mild', 1);
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES('Grinch', 2, 'M', 'white', 'mild', 1);
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES('Wendy', 6, 'F', 'black', 'wild', 1);
-INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES('Null', 20, 'M', 'black', 'mild', 0);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(1,'Mr.Chocolate', 20, 'M', 'black', 'mild', 0);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(2, 'Rowdy', 10, 'M', 'brown', 'mild', 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(3, 'Tabitha', 6, 'F', 'white', 'mild', 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(4, 'Sergent Brown', 19,'M', 'black', 'wild', 0);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(5, 'Melissa', 13, 'F', 'brown', 'mild', 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(6, 'Grinch', 2, 'M', 'white', 'mild', 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(7, 'Wendy', 6, 'F', 'black', 'wild', 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(8, NULL, 20, 'M', 'black', 'mild', 0);
 
 
 
